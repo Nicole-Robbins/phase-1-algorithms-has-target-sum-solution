e1a916d4244dc6320c6e897dccc20196ccce52fd
Nicole Robbins <nicole.robbins@att.net>
2022-11-14 21:16:05 -0600

    commit tree e18933afe90258d8cb481514d2093d4ac910bc13

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seenNumbers = {};
+  for (const number of array) {
+    const complement = target - number;
+    if (seenNumbers[complement]) return true;
+    seenNumbers[number] = true;
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
-  Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
@@ -29,6 +41,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 5, 7, 9, 12], 30));
 }
 
 module.exports = hasTargetSum;
